712944f0b45ef650174742410cff0d351dcebc89
abbirku <abbir.ku@gmail.com>
2020-11-19 01:02:38 +0600

    Add person formatter class

---
+++ Delegate/Formatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    public class Formatter
+    {
+        public string FormatLastName(Person person)
+        {
+            return $"Person last name is ${person.LastName}";
+        }
+
+        public string FormatFirstName(Person person)
+        {
+            return $"Person last name is ${person.FirstName}";
+        }
+
+        public string FormatFullName(Person person)
+        {
+            return $"Person last name is ${person.FirstName} ${person.LastName}";
+        }
+    }
+}
