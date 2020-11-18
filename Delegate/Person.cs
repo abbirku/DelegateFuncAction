f50d3c69343be01f9387d673f890008798c72f87
abbirku <abbir.ku@gmail.com>
2020-11-19 00:51:01 +0600

    ignore .vs

---
+++ Delegate/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
