224057a95904dd79ce622f20c1eb6d4df1245a8f
abbirku <abbir.ku@gmail.com>
2020-11-19 00:59:30 +0600

    Add person class with delegate

--- Delegate/Person.cs
+++ Delegate/Person.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace Delegate
 {
+    public delegate string PersonFormat(Person person);
+
     public class Person
     {
         public string FirstName { get; set; }
@@ -11,7 +13,19 @@ namespace Delegate
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName}";
+            return "Default person string";
+        }
+
+        /// <summary>
+        /// The following method makes person class follow 'Open Close Principle'.
+        /// Because for other ToString formatter, we don't have to create any new method inside this class.
+        /// </summary>
+        public string ToString(PersonFormat formatter)
+        {
+            if(formatter == null)
+                return this.ToString();
+
+            return formatter(this);
         }
     }
 }
